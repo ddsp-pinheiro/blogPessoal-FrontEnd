6e75a903114ae47c9b4161336afb5e56cd89b8b4
ddsp-pinheiro <ddsp.pinheiro@gmail.com>
2021-04-04 17:08:40 -0300

    feat(cadastro/login): consumo de API

--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from '../model/usuario';
+import { UsuarioLogin } from '../model/usuarioLogin';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-cadastrar',
@@ -7,9 +11,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarComponent implements OnInit {
 
-  constructor() { }
+  usuario: Usuario = new Usuario
+
+  confirmarSenha: string
+  tipoUsu: string
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
+    window.scroll(0,0)
   }
+
+  confirmSenha(event:any){
+    this.confirmarSenha = event.target.value
+  }
+
+  tipoUsuario(event:any){
+    this.tipoUsu = event.target.value
+  }
+
+  cadastrar(){
+    this.usuario.tipo = this.tipoUsu
+
+    if(this.usuario.senha != this.confirmarSenha){
+      alert('Senhas diferentes')
+    } else {
+      this.authService.cadastrar(this.usuario).subscribe((resp: Usuario) => {
+        this.usuario = resp
+        this.router.navigate(['/entrar'])
+        alert('Usuário cadastrado com sucesso!')
+      })
+    }
+  }
+
+
 
 }
