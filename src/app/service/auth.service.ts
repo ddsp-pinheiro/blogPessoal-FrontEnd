25751c78b5be1a58e80de934a25d080b98e76397
ddsp-pinheiro <ddsp.pinheiro@gmail.com>
2021-04-05 01:43:09 -0300

    feat(inicio): implementando componente inicio sem consumo

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,3 +1,4 @@
+import { environment } from './../../environments/environment.prod';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
@@ -19,5 +20,15 @@ export class AuthService {
 
   cadastrar(usuario:Usuario):Observable<Usuario> {
     return this.http.post<Usuario>('http://localhost:8080/usuarios/cadastrar', usuario)
+  }
+
+  logado(){
+    let ok = false
+
+    if(environment.token!= ''){
+      ok =true
+    }
+
+    return ok
   }
 }
