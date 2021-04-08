2da36da8fa1f1b5b47161af45958d0b018c382e1
ddsp-pinheiro <ddsp.pinheiro@gmail.com>
2021-04-08 02:00:05 -0300

    feat(delete/edit): implementando put e delete de postagem

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
+import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.component';
 import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { NgModule, Component } from '@angular/core';
@@ -9,15 +11,19 @@ import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
 
 const routes: Routes = [
 
-  { path: '', redirectTo: 'entrar', pathMatch: 'full' },
+  {path: '', redirectTo: 'entrar', pathMatch: 'full'},
 
-  { path: 'entrar', component: EntrarComponent },
-  { path: 'cadastrar', component: CadastrarComponent },
+  {path:'entrar', component: EntrarComponent},
+  {path: 'cadastrar', component: CadastrarComponent},
 
-  { path: 'inicio', component: InicioComponent },
-  { path: 'tema', component: TemaComponent },
-  { path: 'tema-edit/:id', component: TemaEditComponent },
-  { path: 'tema-delete/:id', component: TemaDeleteComponent }
+  {path: 'inicio', component: InicioComponent},
+  {path: 'tema', component: TemaComponent},
+
+  {path: 'tema-edit/:id', component: TemaEditComponent},
+  {path: 'tema-delete/:id', component: TemaDeleteComponent},
+  {path: 'postagem-edit/:id', component: PostagemEditComponent},
+  {path: 'postagem-delete/:id', component: PostagemDeleteComponent}
+
 ];
 
 @NgModule({
